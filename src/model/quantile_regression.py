9460ed6f7e6ccc8d62be78d89ebc317cdc604db4
Dmitriy Lebedinskiy <awesome.lebedinskiy@yandex.ru>
2024-06-25 16:28:40 +0300

    add profit calculator

--- src/model/quantile_regression.py
+++ src/model/quantile_regression.py
@@ -1,5 +1,7 @@
+import time
 from typing import List, Tuple
 
+from joblib import Parallel, delayed
 import numpy as np
 import pandas as pd
 from sklearn.linear_model import QuantileRegressor
@@ -76,13 +78,13 @@ class MultiTargetModel:
         self.targets = [f"next_{horizon}d" for horizon in self.horizons]
         self.fitted_models_ = {}
 
-    def fit(self, data: pd.DataFrame, verbose: bool = False) -> None:
+    def fit(self, data: pd.DataFrame, verbose: bool = False, solver: str = "highs") -> None:
         """Fit model on data.
 
         Parameters
         ----------
         data : pd.DataFrame
-            Data to fit on.
+            Data to fit on.revised simplex
         verbose : bool, optional
             Whether to show progress bar, by default False
             Optional to implement, not used in grading.
@@ -91,16 +93,21 @@ class MultiTargetModel:
         data = data.sort_values(by=[self.date_col, self.sku_col])
         sku_ids = data[self.sku_col].unique()
 
-        for sku_id in sku_ids:
-            data_sku = data[data.sku_id == sku_id]
+        def fit_sku(sku_id):
+            data_sku = data[data[self.sku_col] == sku_id]
             models_for_cur_sku = {}
             for horizon in self.horizons:
                 X = data_sku[self.features]
                 y = data_sku[f"next_{horizon}d"]
                 for q in self.quantiles:
-                    quant_reg = QuantileRegressor(quantile=q, alpha=0)
+                    quant_reg = QuantileRegressor(quantile=q, alpha=0, solver=solver)
                     quant_reg.fit(X, y)
                     models_for_cur_sku[(q, horizon)] = quant_reg
+            return sku_id, models_for_cur_sku
+
+        results = Parallel(n_jobs=-1, verbose=verbose)(delayed(fit_sku)(sku_id) for sku_id in sku_ids)
+
+        for sku_id, models_for_cur_sku in results:
             self.fitted_models_[sku_id] = models_for_cur_sku
 
     def predict(self, data: pd.DataFrame) -> pd.DataFrame:
@@ -127,24 +134,28 @@ class MultiTargetModel:
             "pred_21d_q10", "pred_21d_q50", "pred_21d_q90"
         ]
 
-        res = pd.DataFrame(0, index=data.index, columns=required_columns)
-
-        for sku_id in data[self.sku_col].unique():
+        def predict_sku(sku_id):
             df_sku = data[data[self.sku_col] == sku_id]
-            res.loc[df_sku.index, "sku_id"] = sku_id
-            res.loc[df_sku.index, "day"] = df_sku["day"].dt.date
+            result = pd.DataFrame(0, index=df_sku.index, columns=required_columns)
+            result["sku_id"] = sku_id
+            result["day"] = df_sku[self.date_col].dt.date
+
             for horizon in self.horizons:
                 for quantile in self.quantiles:
                     target_col = f"pred_{horizon}d_q{int(quantile * 100)}"
                     if sku_id in self.fitted_models_:
                         model = self.fitted_models_[sku_id].get((quantile, horizon))
                         if model:
-                            res.loc[df_sku.index, target_col] = model.predict(df_sku[self.features])
+                            result[target_col] = model.predict(df_sku[self.features])
                         else:
-                            res.loc[df_sku.index, target_col] = 0
+                            result[target_col] = 0
                     else:
-                        res.loc[df_sku.index, target_col] = 0
+                        result[target_col] = 0
+            return result
 
+        results = Parallel(n_jobs=-1)(delayed(predict_sku)(sku_id) for sku_id in data[self.sku_col].unique())
+
+        res = pd.concat(results)
         res = res.reset_index(drop=True)
         res = res.sort_values(by=[self.sku_col, self.date_col])
         return res[required_columns]
@@ -173,7 +184,7 @@ def quantile_loss(y_true: np.ndarray, y_pred: np.ndarray, quantile: float) -> fl
     """
     error = y_true - y_pred
     loss = np.maximum(quantile * error, (quantile - 1) * error)
-    return loss
+    return np.mean(loss)
 
 
 def evaluate_model(
@@ -217,6 +228,7 @@ def evaluate_model(
 
 
 if __name__ == "__main__":
+    start = time.time()
     model = MultiTargetModel(
         features=[
             "price",
@@ -243,4 +255,4 @@ if __name__ == "__main__":
 
     predictions = model.predict(df_test)
     print(predictions)
-    # predictions.to_csv("data/pred.csv", index=False)
+    predictions.to_csv("../../data/pred.csv", index=False)
